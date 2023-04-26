645eece1e7e4fc0c66964f4a1ea60c9423d7173b
«Talgat» <tkabysh@gmail.com>
2023-04-26 19:26:24 +0600

    Циклический ввод переменной типа double'

--- Example000/Program.cs
+++ Example000/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double varA;
+while (true)
+{
+  Console.Write("Введите A: ");
+  if (double.TryParse(Console.ReadLine(), out varA))
+    break;
+  Console.WriteLine("Ошибка ввода!");
+}
+Console.WriteLine($"A={varA}");
